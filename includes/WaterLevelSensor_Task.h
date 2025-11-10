3ff3e3f18b35d5615d5b5d167dc468ad5c742153
javonjax <javonj461@hotmail.com>
2025-11-10 23:24:25 +0000

    Water level sensor task now sends its messages to the queue.

--- includes/WaterLevelSensor_Task.h
+++ includes/WaterLevelSensor_Task.h
@@ -1,6 +1,11 @@
+/**
+ *  Datasheet for the water level sensor: hhttps://curtocircuito.com.br/datasheet/sensor/nivel_de_agua_analogico.pdf
+ */
+
 #pragma once
 
 #include "esp_adc/adc_oneshot.h"
+#include <mqtt.h>
 
 // Configure pin number here.
 #define WATER_LEVEL_SENSOR_CHANNEL ADC_CHANNEL_4         // PIN 5
--- main/WaterLevelSensor_Task.c
+++ main/WaterLevelSensor_Task.c
@@ -4,21 +4,25 @@
 
 extern adc_oneshot_unit_handle_t adc1_handle;
 extern adc_cali_handle_t adc_cali_handle;
+extern bool isMQTTConnected;
+extern QueueHandle_t dataQueue;
 
 void vWaterLevelSensor_Task(void *pvParameters) {
     int adc_raw, voltage;
-    char *waterLevel;
+    data_queue_msg_t msg = { .source = WATER_LEVEL_SENSOR };
     while (1) {
         adc_oneshot_read(adc1_handle, WATER_LEVEL_SENSOR_CHANNEL, &adc_raw);
         adc_cali_raw_to_voltage(adc_cali_handle, adc_raw, &voltage);
         if (adc_raw > 750) {
-            waterLevel = "OK";
+            msg.msg_data.waterLevel = "OK";
         } else if (adc_raw > 150) {
-            waterLevel = "LOW";
+            msg.msg_data.waterLevel = "LOW";
         } else {
-            waterLevel = "EMPTY";
+            msg.msg_data.waterLevel = "EMPTY";
         }
-        printf("Current water level: %s\n", waterLevel);
-        vTaskDelay(pdMS_TO_TICKS(200));
+        if (isMQTTConnected) {
+                xQueueSend(dataQueue, &msg, portMAX_DELAY);
+        }
+        vTaskDelay(pdMS_TO_TICKS(20000));
     }
 };
